fab7bba122abc5a3024ebe5623ed1c4a5222d03f
aridari-1 <proposalcampaign@gmail.com>
2026-04-24 17:44:56 -0500

    trap added to upload

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -158,6 +158,7 @@ export default function UploadPage() {
         <option>Rumba</option>
         <option>Drill</option>
         <option>Maimouna</option>
+          <option>Trap</option>
       </select>
 
       {/* AUDIO */}
